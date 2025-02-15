9d55307722314068703ff0cf7a94d81d96731773
Dev-Tonia <ogunwaanthonia@gmail.com>
2025-02-15 09:36:08 +0100

    fix: hosting path not found

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import TicketSelection from "../components/events/ticketSelection";
 import EventCard from "../components/events/EventCard";
 
 export default function Events() {
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-import path from "path";
+// Create __dirname equivalent in ES modules
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
